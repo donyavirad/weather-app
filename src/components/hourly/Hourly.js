d022c0515151ba8e9558bdbd4cef11e217e1378d
hamed <hameddonyavi81@gmail.com>
2022-04-08 12:09:21 +0430

    test2

--- src/components/hourly/Hourly.js
+++ src/components/hourly/Hourly.js
@@ -12,7 +12,6 @@ const Hourly = (props)=>{
                     </div>
                     <div className="hourly__content">
                         <div className="hourly__content-scroll">
-                            <a>test</a>
                             <div className="hourly__content-grid">
                                 <div className="hourly__content-item" id="start">
                                     <p className="hourly__content-time">09:43</p>
