370620ffee3cc64f018847324a63a6eb3fea7cf3
hamed <hameddonyavi81@gmail.com>
2022-04-15 17:21:15 +0430

    .

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,15 @@
 import React from "react"
 import "./Navbar.scss"
+import { Link } from "react-router-dom"
 import menuIcon from "../../assets/images/menu.svg"
 const Navbar = ()=>{
     return(
-        <div className="navbar">
-            <img className="navbar__image" src={menuIcon} alt=""/>
-            <p className="navbar__text">گزینه ها</p>
-        </div>
+        <Link to="/start">
+            <div className="navbar">
+                <img className="navbar__image" src={menuIcon} alt=""/>
+                <p className="navbar__text">گزینه ها</p>
+            </div>
+        </Link>
     )
 }
 
